761d3d9390f5be91c491033514fe1cd8c61155a3
Jarosław Bagnicki <jaroslaw.bagnicki@medius.com>
2023-04-11 11:27:29 +0200

    Complete script

---
+++ scripts/gh-api/Get-PlaylistVideos.ps1
@@ -0,0 +1,74 @@
+param(
+    [Parameter(Mandatory=$True)]
+    [string]$PlaylistId
+)
+
+$ErrorActionPreference = "Stop"
+
+$config = Import-PowerShellDataFile -Path (Join-Path $PSScriptRoot ".\config.psd1")
+
+$playlistItemsPageToken = $null
+$output = @()
+do {
+    # https://developers.google.com/youtube/v3/docs/playlistItems/list
+    $playlistItemsRequest = @{
+        Method = "GET"
+        Uri = "$($config.ApiUrl)/playlistItems"
+        Body = @{
+            key = $config.ApiKey
+            playlistId = $PlaylistId
+            part = "snippet"
+            maxResults = 50
+            pageToken = $playlistItemsPageToken
+        }
+    }
+
+    $playlistItemsResponse = Invoke-WebRequest @playlistItemsRequest
+
+    if ($response.StatusCode -ge 400) {
+        Write-Error $response.Content
+        exit -1
+    }
+
+    $playlistItemsResult = ConvertFrom-Json $playlistItemsResponse.Content
+    $playlistItemsPageToken = $playlistItemsResult.NextPageToken
+
+    $videoIds = $playlistItemsResult.Items.Snippet.ResourceId.VideoId
+    # https://developers.google.com/youtube/v3/docs/videos/list
+    $videosRequest = @{
+        Method = "GET"
+        Uri = "$($config.ApiUrl)/videos"
+        Body = @{
+            key = $config.ApiKey
+            id = $videoIds -join ","
+            part = "contentDetails"
+            maxResults = 50
+        }
+    }
+
+    $videosResponse = Invoke-WebRequest @videosRequest
+    if ($response.StatusCode -ge 400) {
+        Write-Error $response.Content
+        exit -1
+    }
+
+    $videosResult = ConvertFrom-Json $videosResponse.Content
+
+    foreach ($playlistItem in $playlistItemsResult.Items.Snippet)
+    {
+        $videoId = $playlistItem.ResourceId.VideoId
+        $videoDetails = $videosResult.Items | Where-Object Id -eq $videoId
+        
+        $entry = [PSCustomObject]@{
+            VideoId = $videoId
+            Position = $playlistItem.Position
+            Title = $playlistItem.Title
+            Duration = [System.Xml.XmlConvert]::ToTimeSpan($videoDetails.ContentDetails.Duration)
+        }
+        $output += $entry
+    }
+} while ($null -ne $playlistItemsPageToken)
+
+$output | ft
+
+
---
+++ scripts/gh-api/README.md
@@ -0,0 +1,3 @@
+```powershell
+.\Get-PlaylistVideos -PlaylistId "PLxfrSxK7P38X7XfG4X8Y9cdOURvC7ObMF"
+```
